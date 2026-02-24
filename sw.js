48058d7b30d8b193ed78bd342a1e280ccdf1d308
nofadin <62503898+nofadin@users.noreply.github.com>
2026-02-24 22:15:08 +0700

    Implement service worker caching for assets

--- sw.js
+++ sw.js
@@ -1,8 +1,20 @@
-// sw.js
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
+const CACHE_NAME = 'mantap-ksc-v1';
+const urlsToCache = [
+  '/MANTAP_Ver.001/',
+  '/MANTAP_Ver.001/index.html',
+  '/MANTAP_Ver.001/manifest.json',
+  '/MANTAP_Ver.001/icon-192.png',
+  '/MANTAP_Ver.001/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
